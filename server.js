578acf389a7c583a320d850f26ecc175c4e7b256
Roomdada <dsieroger@gmail.com>
2022-11-09 06:13:21 +0000

    fix

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+
+app.listen(3000);
